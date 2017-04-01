dbf9015761b8c1d2a43075e78f0979cfcf114b3b
Gabriel Munumel <gabrielmunumel@gmail.com>
2017-04-01 16:55:59 +0200

    Added first working version of Client-side and socket.io :)

--- client-side/src/app/app.module.ts
+++ client-side/src/app/app.module.ts
@@ -1,20 +1,15 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
-import { AppComponent } from './app.component';
-
+import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
+import { AppComponent }  from './app.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        FormsModule // <-- import the FormsModule before binding with [(ngModel)]
+    ],
+    declarations: [
+        AppComponent
+    ],
+    bootstrap: [ AppComponent ]
 })
 export class AppModule { }
