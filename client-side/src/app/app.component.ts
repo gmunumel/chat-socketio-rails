01e0e42d33c01fa43127759db63d255bfe314700
Gabriel Munumel <gabrielmunumel@gmail.com>
2017-04-07 19:51:37 +0200

    Added Client-Side first version

--- client-side/src/app/app.component.ts
+++ client-side/src/app/app.component.ts
@@ -1,49 +1,7 @@
 import { Component } from '@angular/core';
-import { SocketService } from './services/socket.service';
 
-
-export class Hero {
-    id: number;
-    name: string;
-}
 @Component({
-    selector: 'my-app',
-    template: `
-    <h1>{{title}}</h1>
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name">
-    </div>
-    `
+  selector: 'my-app',
+  template: `<h1>Hello {{name}}</h1>`,
 })
-export class AppComponent {
-    private socketService: SocketService;
-    title = 'Tour of Heroes';
-    hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
-    };
-
-    constructor() {
-        this.socketService = new SocketService();
-        this.socketService.get();
-    }
-
-    // socket: SocketIOClient.Socket;
-
-    // constructor() {}
-    //
-    // get(): any {
-    //     this.socket = io.connect('localhost:5001');
-    //
-    // }
-
-    //socket: SocketIOClient.Socket;
-
-    // socket.on('users-kist', function(user){
-    //     alert('Welcome ' + user);
-    // });
-
-}
+export class AppComponent  { name = 'Angular'; }
--- client-side/src/app/app.module.ts
+++ client-side/src/app/app.module.ts
@@ -1,15 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
+
 import { AppComponent }  from './app.component';
+
 @NgModule({
-    imports: [
-        BrowserModule,
-        FormsModule // <-- import the FormsModule before binding with [(ngModel)]
-    ],
-    declarations: [
-        AppComponent
-    ],
-    bootstrap: [ AppComponent ]
+  imports:      [ BrowserModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
