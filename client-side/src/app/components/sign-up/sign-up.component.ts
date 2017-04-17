c012f79beba2248355597b84819df3806133c67c
Gabriel Munumel <gabrielmunumel@gmail.com>
2017-04-17 21:08:53 +0200

    Removed comments

--- client-side/src/app/components/sign-up/sign-up.component.ts
+++ client-side/src/app/components/sign-up/sign-up.component.ts
@@ -9,18 +9,13 @@ const emailRegex = '^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{
 @Component({
   selector: 'sign-up',
   templateUrl: './sign-up.component.html',
-  providers: [UserService]
+  providers: [ UserService ]
 })
 export class SignUpComponent {
   page: string = 'Sign Up';
   response: number = 0;
   user: User;
   signUpForm: FormGroup;
-
-  // public signUpForm = this.fb.group({
-  //   name: ['', Validators.required],
-  //   email: ['', [Validators.required, Validators.pattern(emailRegex)]],
-  // });
 
   constructor(
     private fb: FormBuilder,
