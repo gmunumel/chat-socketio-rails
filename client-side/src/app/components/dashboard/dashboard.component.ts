46b10afec9b12767a951b47eb0b3f62607701937
Gabriel Munumel <gabrielmunumel@gmail.com>
2017-04-24 23:07:09 +0200

    Added first version of Dashboard Feel and look

--- client-side/src/app/components/dashboard/dashboard.component.ts
+++ client-side/src/app/components/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'dashboard',
   templateUrl: './dashboard.component.html',
+  styleUrls: [ './dashboard.component.css' ]
 })
 export class DashboardComponent {
   page: string = 'Dashboard';
