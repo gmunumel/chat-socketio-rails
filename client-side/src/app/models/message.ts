e631bb510fe56a9e967c08a3a02ac1d5d7fcfd11
Gabriel Munumel <gabrielmunumel@gmail.com>
2017-05-20 17:11:23 +0200

    Added created_at field to message model

--- client-side/src/app/models/message.ts
+++ client-side/src/app/models/message.ts
@@ -3,6 +3,7 @@ export class Message {
     public id = 0,
     public body = '',
     public user_id = 1,
-    public chat_room_id = 1) { }
+    public chat_room_id = 1,
+    public created_at: Date = null) { }
   clone() { return new Message(this.id, this.body, this.user_id, this.chat_room_id); }
 }
