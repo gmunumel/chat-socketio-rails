968dc69e93ea603a18a0b540d72596d3a78a2ef5
Gabriel Munumel <gabrielmunumel@gmail.com>
2017-06-03 18:08:27 +0200

    Clean up client-side app

--- client-side/src/main.ts
+++ client-side/src/main.ts
@@ -1,5 +1,12 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import { enableProdMode } from '@angular/core';
+
 import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
+
+// Enable production mode unless running locally
+if (!/localhost/.test(document.location.host)) {
+  enableProdMode();
+}
